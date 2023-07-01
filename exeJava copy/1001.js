0913b68acac64831dd25b843d1f038294605623f
Matheus Souza <goncalvesouza1996@gmail.com>
2023-07-01 12:39:34 -0300

    First Commit

---
+++ exeJava copy/1001.js
@@ -0,0 +1,8 @@
+const readline = require('readline-sync');
+
+const a = parseInt(readline.question('Primeiro valor: '));
+const b = parseInt(readline.question('Segundo valor: '));
+
+const x = a + b;
+
+console.log(x);
