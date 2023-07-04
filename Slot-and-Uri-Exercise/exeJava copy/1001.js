2e2b7d05cbbdfe7ea6d8dc7b70a15368dafecb57
Matheus Gonçalves Caicó <goncalvesouza1996@gmail.com>
2023-07-04 20:14:32 -0300

    subindo atualiazação do js e expo

--- Slot-and-Uri-Exercise/exeJava copy/1001.js
+++
@@ -1,8 +0,0 @@
-const readline = require('readline-sync');
-
-const a = parseInt(readline.question('Primeiro valor: '));
-const b = parseInt(readline.question('Segundo valor: '));
-
-const x = a + b;
-
-console.log(x);
